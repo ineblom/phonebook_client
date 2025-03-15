a91c87afdc4bf10658f08deafe8b837dbee437f9
Theodor Ineblom <theodor.ineblom@gmail.com>
2025-03-15 22:12:37 +0100

    specify custom colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0091ff",
+        secondary: "#022c1a",
+        light: {
+          100: "#c6dbff",
+          200: "#a8c6db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#ab8bff",
+      }
+    },
   },
   plugins: [],
 }
